a681649f87d65b67cbdf5f559300f825e8eb2c15
Anton <antonfrolov1977@gmail.com>
2026-05-25 19:52:14 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,49 @@
 // Copyright 2022 NNTU-CS
+#include <fstream>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  std::ofstream fout("data.txt");
 
-  while (count--)
-    train.addCar(false);
+  srand(time(nullptr));
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  for (int n = 10; n <= 500; n += 10) {
+    Train t1;
+    Train t2;
+    Train t3;
+
+    // все выключены
+    for (int i = 0; i < n; i++) {
+      t1.addCar(false);
+    }
+
+    // все включены
+    for (int i = 0; i < n; i++) {
+      t2.addCar(true);
+    }
+
+    // случайные лампочки
+    for (int i = 0; i < n; i++) {
+      t3.addCar(rand() % 2);
+    }
+
+    t1.getLength();
+    t2.getLength();
+    t3.getLength();
+
+    fout << n << " "
+         << t1.getOpCount() << " "
+         << t2.getOpCount() << " "
+         << t3.getOpCount() << std::endl;
+
+    std::cout << n << std::endl;
+  }
+
+  fout.close();
+
   return 0;
 }
